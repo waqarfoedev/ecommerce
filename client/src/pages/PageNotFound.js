16bc277fbcb51e3bf66fb3b9e3cccea2c07e1c1b
waqarfoedev <122679329+waqarfoedev@users.noreply.github.com>
2023-03-24 12:13:22 +0500

    SEO

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
 
 const PageNotFound = () => {
   return (
-    <Layout>
+    <Layout title={"404 page not found "}>
       <div className='pnf'>
-<h1 className='pnf-title'>404</h1>
-<h2 className='pnf-heading' >Oops ! Page Not Found</h2>
-<Link to='/' className='pnf-btn'>Go Back</Link>
+        <h1 className='pnf-title'>404</h1>
+        <h2 className='pnf-heading' >Oops ! Page Not Found</h2>
+        <Link to='/' className='pnf-btn'>Go Back</Link>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
