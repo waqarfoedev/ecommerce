eab5ffffab410c4036b073a00e96c2a24e925b2f
waqarfoedev <122679329+waqarfoedev@users.noreply.github.com>
2023-04-08 19:59:16 +0500

    add photo, get product and delete product

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -45,3 +45,91 @@ export const createProductsController = async (req, res) => {
         });
     }
 };
+
+// get all products
+export const getProductsController = async (req, res) => {
+    try {
+        const products = await productModels
+            .find({})
+            .populate('category')
+            .select('-photo')
+            .limit(12).
+            sort({ createdAt: -1 });
+        res.status(200).send({
+            success: true,
+            message: " all products",
+            products
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error while get all products",
+            error
+        });
+    }
+};
+
+//get Single products
+export const getSingleProductsController = async (req, res) => {
+    try {
+        const { slug } = req.params;
+        const product = await productModels
+            .findOne({ slug })
+            .populate('category')
+            .select('-photo');
+        res.status(200).send({
+            success: true,
+            message: " get single product",
+            product
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error while get single products",
+            error
+        });
+    }
+};
+
+//get photo 
+export const productsPhotoController = async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const product = await productModels.findById(pid).select('photo');
+
+        if (product.photo.data) {
+            res.set('Content-type', product.photo.contentType);
+            return res.status(200).send(product.photo.data);
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error while get photo",
+            error
+        });
+    }
+};
+
+//delete product
+export const deleteProductController = async (req, res) => {
+    try {
+        const { pid } = req.params;
+        await productModels.findByIdAndDelete(pid).select('-photo');
+        res.status(200).send({
+            success: true,
+            message: " product deleted successfully",
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error while deleting product",
+            error
+        });
+    }
+};
--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import { isAdmin, requireSignIn } from '../middelwares/middelwares.js';
-import { createProductsController } from '../controllers/productsController.js';
+import {
+    createProductsController,
+    getProductsController,
+    getSingleProductsController,
+    productsPhotoController,
+    deleteProductController,
+} from '../controllers/productsController.js';
 import formidable from 'express-formidable';
 
 const router = express.Router();
 
 // routes
 
+// create product
 router.post('/create-products', requireSignIn, isAdmin, formidable(), createProductsController);
 
+// get all
+router.get('/get-products', getProductsController);
+
+//grt single product
+router.get('/get-products/:slug', getSingleProductsController);
+
+//get photo
+router.get('/product-photo/:pid', productsPhotoController);
+
+//delete product
+router.delete('/product/:pid', deleteProductController);
 
 export default router;
