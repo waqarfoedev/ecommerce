7b88d67d3c1082c549bbbfa90139733effa01d6e
waqarfoedev <waqarunirollno1701831@gmail.com>
2023-05-08 20:29:03 +0500

    add detail button

--- client/src/pages/Auth/ProductCard.js
+++ client/src/pages/Auth/ProductCard.js
@@ -2,15 +2,20 @@ import React from "react";
 import "../../styles/ProductStyle.css";
 import { MdFavorite } from "react-icons/md";
 import { IoCartSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ p }) => {
   return (
     <div className="product-card">
       <div className="product-tumb">
         <img src={`/api/v1/products/product-photo/${p._id}`} alt={p.name} />
+        <div className="middle">
+          <Link className="text">Details.</Link>
+        </div>
       </div>
       <div className="product-details">
-        <span className="product-catagory">{p.name}</span>
+        <span className="product-name">{p.name}</span>
+        <span className="product-catagory">{p.category.name}</span>
 
         <div className="product-bottom-details">
           <div className="product-price">${p.price}</div>
